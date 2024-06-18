0417fb6b693fa1992087408db43f708ab83626af
fozia <foziakassa19@gmail.com>
2024-06-18 21:41:44 +0300

    updated1

--- gemini-clone/src/component/Mainn/Mainn.jsx
+++ gemini-clone/src/component/Mainn/Mainn.jsx
@@ -1,39 +1,54 @@
 import React from "react";
-import './Mainn.css'
-import {assets} from '../../assets/assets'
-const Mainn=()=>{
-    return(
-        <div className="main">
-            <div className="nav">
-                <p>Gemini</p>
-                <img src={assets.user_icon} alt="" />
-            </div>
-            <div className="main-container">
-                <div className="greet">
-                    <p><span>hello</span></p>
-                    <p>heow can i help you</p>
-                </div>
-                <div className="cards">
-                    <div className="card">
-                        <p>Lorem ipsum dolor sit ame</p>
-                        <img src={assets.compass_icon} alt="" />
-                    </div>
-                    <div className="card">
-                        <p>Lorem ipsum dolor sit ame</p>
-                        <img src={assets.bulb_icon} alt="" />
-                    </div>
-                    <div className="card">
-                        <p>Lorem ipsum dolor sit ame</p>
-                        <img src={assets.message_icon} alt="" />
-                    </div>
-                    <div className="card">
-                        <p>Lorem ipsum dolor sit ame</p>
-                        <img src={assets.code_icon} alt="" />
-                    </div>
-                    
-                </div>
-            </div>
+import "./Mainn.css";
+import { assets } from "../../assets/assets";
+const Mainn = () => {
+  return (
+    <div className="main">
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="" />
+      </div>
+      <div className="main-container">
+        <div className="greet">
+          <p>
+            <span>hello</span>
+          </p>
+          <p>heow can i help you</p>
         </div>
-    )
-}
+        <div className="cards">
+          <div className="card">
+            <p>Lorem ipsum dolor sit ame</p>
+            <img src={assets.compass_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Lorem ipsum dolor sit ame</p>
+            <img src={assets.bulb_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Lorem ipsum dolor sit ame</p>
+            <img src={assets.message_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Lorem ipsum dolor sit ame</p>
+            <img src={assets.code_icon} alt="" />
+          </div>
+        </div>
+        <div className="main-bottom">
+            <div className="search-box">
+                <input type="text" placeholder="Enter" />
+                <div className="">
+                    <img src={assets.gallery_icon} alt="" />
+                    <img src={assets.mic_icon} alt="" />
+                    <img src={assets.send_icon}alt="" />
+                </div>
+            </div>
+            <p className="bottom-info">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis, aut quo neque odio ut eligen
+            </p>
+
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Mainn;
---
+++ gemini-clone/src/config/gemini.js
@@ -0,0 +1,45 @@
+// AIzaSyAcR2i-2X36rZFvQBn7Dv86ZXL5dRDU26A
+/*
+ * Install the Generative AI SDK
+ *
+ * $ npm install @google/generative-ai
+ *
+ * See the getting started guide for more information
+ * https://ai.google.dev/gemini-api/docs/get-started/node
+ */
+
+const {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} = require("@google/generative-ai");
+
+const apiKey = process.env.GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run() {
+  const chatSession = model.startChat({
+    generationConfig,
+ // safetySettings: Adjust safety settings
+ // See https://ai.google.dev/gemini-api/docs/safety-settings
+    history: [
+    ],
+  });
+
+  const result = await chatSession.sendMessage("INSERT_INPUT_HERE");
+  console.log(result.response.text());
+}
+
+run();
